7a028b77c6bb6f52b50ea1c34870844e7621a25b
Maykmora <maykmora12@gmail.com>
2025-07-25 09:44:27 -0600

    Commit 4

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,20 +1,18 @@
-def menu():
+productos=[]
+while True:
     print("\n--MENÚ--")
     print("1.Agregar un producto a la lista")
     print("2.Modificar un producto existente")
     print("3.Eliminar un producto")
     print("4.Ver todos los productos")
     print("5.Salir del programa")
-
-while True:
-    productos=[]
-    menu()
     option=input("Seleccione una opción del menú (1-5): ")
 
     match option:
         case "1":
             p=input("Ingrese un producto a la lista: ")
             productos.append(p)
+            print("Producto agregado")
         case "2":
             print(f"La lista de sus productos es:{productos}")
             indice=int(input("Ingrese el numero de indice del producto que quiere remplazar:"))
